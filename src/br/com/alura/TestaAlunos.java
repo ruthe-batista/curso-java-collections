9ae1c5dbe9f83799787c171f1ca7695185b4206d
ruthe-batista <ruthebatista04@gmail.com>
2022-01-06 09:36:46 -0300

    Uma nova colecao o set

---
+++ src/br/com/alura/TestaAlunos.java
@@ -0,0 +1,37 @@
+package br.com.alura;
+
+import java.util.*;
+
+public class TestaAlunos {
+
+    public static void main(String[] args) {
+
+        Set<String> alunos = new HashSet<>();
+        alunos.add("Rodrigo Turini");
+        alunos.add("Alberto Souza");
+        alunos.add("Nico Steppat");
+        alunos.add("Sergio Lopes");
+        alunos.add("Renan Saggio");
+        alunos.add("Mauricio Aniche");
+
+        boolean pauloEstaMatriculado = alunos.contains("Paulo Silveira");
+        alunos.remove("Sergio Lopes");
+
+        System.out.println(pauloEstaMatriculado);
+
+        System.out.println(alunos.size());
+
+        for (String aluno: alunos) {
+            System.out.println(aluno);
+        }
+
+        alunos.forEach(aluno ->{
+            System.out.println(alunos);
+        });
+
+        System.out.println(alunos);
+
+        List<String> alunosEmLista = new ArrayList<>(alunos);
+
+    }
+}
